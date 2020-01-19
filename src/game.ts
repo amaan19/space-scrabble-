5d4dc82875f0e7409bcb4b1f98a04fe60ce066d9
Mani <amaan.ahmed3@gmail.com>
2020-01-19 10:08:17 +0000

    Configured webpack to run TS

--- src/game.ts
+++ src/game.ts
@@ -2,7 +2,5 @@ import { Physics } from "./app/Physics";
 import { Renderer } from "./app/Renderer";
 import { Game } from "./app/App";
 
-const physics = new Physics();
-const renderer = new Renderer();
 const game = new Game();
 game.start();
